6aa3a1463cc12b0efab9ac41315d3cd6527ab918
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-01-22 19:37:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let nazira =prompt('Ismingizni kiriting');
+let broke =prompt('Space IDingizni kiriting');
+let hurt =prompt('Space passwordingizni kiriting');
+
+
+console.log({
+
+name:(`${nazira}`),
+Id:(`${broke}`),
+password:(`${hurt}`)
+});
